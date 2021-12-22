cbde0f34b5ac9317f4f7c3c87cd529808a9719b5
yoshiki nagahamaya <yoshiki.nagahamaya@epkotsoftware.co.jp>
2021-12-22 16:25:44 +0900

    laravel初期設定

--- env/laravel8/app/app/Models/User.php
+++ env/laravel8/app/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -20,8 +21,6 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'locked_flg',
-        'error_count',
     ];
 
     /**
@@ -31,6 +30,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
+        'remember_token',
     ];
 
     /**
@@ -38,61 +38,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    /**protected $casts = [
+    protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    */
-    /**
-     * Emailがマッチしたユーサを返す
-     * @@param string $email
-     * @return object
-     */
-    public function getUserByEmail($email){
-        return User::where('email','=',$email)->first();
-    }
-
-    /**
-     * アカウントがロックされているか？
-     * @param object $user
-     * @return bool
-     */
-    public function isAccountLocked($user){
-        if($user->locked_flg === 1){
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * エラーカウントをリセットする
-     * @param object $user
-     * @return int
-     */
-    public function resetErrorCount($user){
-        if ($user->error_count > 0){
-            $user->error_count = 0;
-            $user->save();
-        }
-    }
-
-    /**
-     * エラーカウントを１増やす
-     * @param int $error_count
-     */
-    public function addErrorCount($error_count){
-        return $error_count + 1;
-    }
-
-    /**
-     * アカウントをロックする
-     * @param object $user
-     * @return bool
-     */
-    public function lockAccount($user){
-        if ($user->error_count > 5){
-            $user->locked_flg = 1;
-            return $user->save();
-        }
-    return false;
-    }
 }
--- env/laravel8/app/routes/web.php
+++ env/laravel8/app/routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,24 +13,6 @@ use App\Http\Controllers\Auth\AuthController;
 |
 */
 
-Route::group(['middleware' => ['guest']], function(){
-    //新規登録
-    Route::get('/register', [AuthController::class,'showRegister'])->name('register.show');
-    //登録処理
-    Route::post('/register/register',[AuthController::class, 'store'])->name('register');
-    //ログインフォーム表示
-    Route::get('/', [AuthController::class,'showLogin'])->name('login.show');
-    //ログイン処理
-    Route::post('/login', [AuthController::class,'login'])->name('login');
-});
-
-Route::group(['middleware' => ['auth']], function(){
-    //ホーム画面
-    Route::get('/home',function(){
-        return view('home');
-    })->name('home');
-    //create画面
-    Route::get('/create',HomeController::class,'')
-    //ログアウト
-    Route::post('logout',[AuthController::class,'logout'])->name('logout');
+Route::get('/', function () {
+    return view('welcome');
 });
