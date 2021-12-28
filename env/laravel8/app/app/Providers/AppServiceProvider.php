e1e5276c21f9c8b6c21f3bcd916192ca735996d5
yoshiki nagahamaya <yoshiki.nagahamaya@epkotsoftware.co.jp>
2021-12-28 10:54:09 +0900

    laravel-note

--- env/laravel8/app/app/Providers/AppServiceProvider.php
+++ env/laravel8/app/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Memo;
+use App\Models\Tag;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // すべてのメソッドが呼ばれる前に先に呼ばれるメソッド
+        view()->composer('*', function ($view) {
+            // get the current user
+            $user = \Auth::user();
+             // インスタンス化
+            $memoModel = new Memo();
+            $memos = $memoModel->myMemo( \Auth::id() );
+
+            // タグに取得
+            $tagModel = new Tag();
+            $tags = $tagModel->where('user_id', \Auth::id())->get();
+
+            $view->with('user', $user)->with('memos', $memos)->with('tags', $tags);
+        });
     }
 }
